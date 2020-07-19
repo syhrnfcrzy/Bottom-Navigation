92967ac33a863928c04e36f1de145bb0d1693860
syhrn <syahrunfacrezy@gmail.com>
2020-07-20 08:46:39 +0900

    Helllo-5

---
+++ app/src/main/java/com/ezy/bottomnavigation/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ezy.bottomnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
